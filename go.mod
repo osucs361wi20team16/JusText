76fbeb5facd2baa67b7f10cc87acda918ed402ab
Nick Meyer <nick@e3b0c442.io>
2020-01-30 19:25:08 -0600

    hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/osucs361wi20team16/justext
 
 go 1.13
+
+require github.com/rivo/tview v0.0.0-20200129065529-3a5c6317e432
--- justext.go
+++ justext.go
@@ -1,5 +1,10 @@
 package justext
 
-func Run() {
+import "github.com/rivo/tview"
 
+func Run() {
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
