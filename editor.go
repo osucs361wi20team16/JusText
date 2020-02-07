0a59e67d8e8ba85b834f25f311953d0d8a21c60b
Xynda <doanan@oregonstate.edu>
2020-02-06 19:40:58 -0600

    Implementation of editText

--- editor.go
+++ editor.go
@@ -1,7 +1,7 @@
 package justext
 
 import (
-	"unicode/utf8"
+	"strings"
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
@@ -10,13 +10,24 @@ import (
 func EditorView() *tview.TextView {
 	State.TextView = tview.NewTextView()
 	State.TextView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		R := event.Rune()
-		B := make([]byte, 1)
-		utf8.EncodeRune(B, R)
-		State.TextView.Write(B)
+		switch event.Key() {
+		case tcell.KeyBS:
+			if len(State.Buffer) == 0 {
+				break
+			}
+			RS := []rune(State.Buffer)
+			RS = RS[:len(RS)-1]
+			State.Buffer = string(RS)
+		default:
+			SB := &strings.Builder{}
+			SB.WriteString(State.Buffer)
+			SB.WriteRune(event.Rune())
+			State.Buffer = SB.String()
+		}
+		State.TextView.SetText(State.Buffer)
 		State.App.Draw()
 		return nil
 	})
-	State.TextView.Write([]byte("Hello, World!"))
+	State.TextView.SetText("")
 	return State.TextView
 }
