7d88652fcf914b590e0fbbb85f04c3d5ae49bcbb
Xynda <doanan@oregonstate.edu>
2020-02-06 19:14:30 -0600

    Added Ability to type in terminal

--- editor.go
+++ editor.go
@@ -1,9 +1,22 @@
 package justext
 
-import "github.com/rivo/tview"
+import (
+	"unicode/utf8"
+
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
 
 func EditorView() *tview.TextView {
 	State.TextView = tview.NewTextView()
+	State.TextView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		R := event.Rune()
+		B := make([]byte, 1)
+		utf8.EncodeRune(B, R)
+		State.TextView.Write(B)
+		State.App.Draw()
+		return nil
+	})
 	State.TextView.Write([]byte("Hello, World!"))
 	return State.TextView
 }
