c3f310647e8d75a8709c4cfbabcdb3a0d6c73c23
Nick Meyer <nick@e3b0c442.io>
2020-02-10 20:40:15 -0600

    change internal buffer from string to rune slice

--- justext.go
+++ justext.go
@@ -3,33 +3,34 @@ package justext
 import "github.com/rivo/tview"
 
 type EditorState struct {
-	Buffer   string
-	App      *tview.Application
-	TextView *tview.TextView
-	Cursor   int
-	menuGrid *tview.Grid
-	maingrid *tview.Grid
-	Filename string
+	Buffer           []byte
+	App              *tview.Application
+	TextView         *tview.TextView
+	Cursor           int
+	MenuGrid         *tview.Grid
+	MainGrid         *tview.Grid
+	Filename         string
 	SwitchMenuColumn bool
 }
 
 var State EditorState
 
 func Run() {
-	State = EditorState{}
-	State.SwitchMenuColumn = false
-	State.Filename = "test.txt"
-	State.maingrid = tview.NewGrid().
-		SetRows(1, 0, 1).
-		SetBorders(true).
-		AddItem(MenuBarView(), 0, 0, 1, 1, 1, 1, false).
-		AddItem(EditorView(), 1, 0, 1, 1, 1, 1, true).
-		AddItem(StatusBarView(), 2, 0, 1, 1, 1, 1, false)
+	State = EditorState{
+		SwitchMenuColumn: false,
+		Filename:         "text.txt",
+		MainGrid: tview.NewGrid().
+			SetRows(1, 0, 1).
+			SetBorders(true).
+			AddItem(MenuBarView(), 0, 0, 1, 1, 1, 1, false).
+			AddItem(EditorView(), 1, 0, 1, 1, 1, 1, true).
+			AddItem(StatusBarView(), 2, 0, 1, 1, 1, 1, false),
+	}
 
 	State.App = tview.NewApplication()
 
 	if err := State.App.
-		SetRoot(State.maingrid, true).
+		SetRoot(State.MainGrid, true).
 		SetFocus(State.TextView).
 		Run(); err != nil {
 		panic(err)
